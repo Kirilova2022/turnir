f07fa17735c1c368d16c6d1c48d416347cd69278
Kirilova2022 <104797895+Kirilova2022@users.noreply.github.com>
2022-10-07 23:23:02 +0300

    Update Player.java

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,2 +1,37 @@
-package domain;public class Player {
+package ru.netology.Tournament;
+
+public class Player {
+    protected int id;
+    protected String playerName;
+    protected int strength;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+    public void setName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public Player(int id, String playerName, int strength) {
+        this.id = id;
+        this.playerName = playerName;
+        this.strength = strength;
+    }
 }
